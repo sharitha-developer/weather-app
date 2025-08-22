3f987e7667f8d707c4bdbca6b658a9701a0a6f66
sharitha-developer <sharitha.developer@gmail.com>
2025-08-22 18:37:37 +1000

    weather app finished

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,52 +1,150 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import sunny from "../assets/images/sunny.png";
-import cloudy from "../assets/images/cloudy.png"
-import rainy from "../assets/images/rainy.png"
-import snowy from "../assets/images/snowy.png"
+import cloudy from "../assets/images/cloudy.png";
+import rainy from "../assets/images/rainy.png";
+import snowy from "../assets/images/snowy.png";
+import loadingGif from "../assets/images/loading.gif";
 
 const WeatherApp = () => {
-    const api_key = "c3181bfffa20b28892497ddb04f48729" ;
-    const search = () => {
-        const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=api_key' ;
-    } 
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [loading, setLoading] = useState(false);
+  const api_key = "c3181bfffa20b28892497ddb04f48729";
+
+  useEffect(() => {
+    const fetchDefaultWeather = async () => {
+      setLoading(true);
+      const defaultLocation = "Traralgon";
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=${api_key}`;
+      const res = await fetch(url);
+      const searchData = await res.json();
+      setData(searchData);
+      setLocation("");
+      setLoading(false);
+    };
+    fetchDefaultWeather();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+  const search = async () => {
+    if (location.trim() !== "") {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`;
+      const res = await fetch(url);
+      const searchData = await res.json();
+      if (searchData.cod !== 200) {
+        setData({ notFound: true });
+        return;
+      }
+      console.log(searchData);
+      setData(searchData);
+      setLocation("");
+      setLoading(false);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  const weatherImages = {
+    Clear: sunny,
+    Clouds: cloudy,
+    Rain: rainy,
+    Snow: snowy,
+    Haze: cloudy,
+    Mist: cloudy,
+  };
+
+  const weatherImage =
+    weatherImages[data.weather ? data.weather[0].main : "null"];
+
+  const backgroundImages = {
+    Clear: "linear-gradient(to right, #f3b07c, #fcd283)",
+    Clouds: "linear-gradient(to right, #57d6d4, #71eeec)",
+    Rain: "linear-gradient(to right, #5bc8fb, #80eaff)",
+    Snow: "linear-gradient(to right, #aff2ff, #ffffff)",
+    Haze: "linear-gradient(to right, #57d6d4, #71eeec)",
+    Mist: "linear-gradient(to right, #57d6d4, #71eeec)",
+  };
+  const backgroundImage =
+    backgroundImages[data.weather ? data.weather[0].main : "Clear"];
+
+  const currentDate = new Date().toLocaleDateString("en-GB", {
+    weekday: "short", // Fri
+    day: "numeric", // 3
+    month: "short", // May
+  });
+
   return (
-    <div className='container'>
-        <div className="weather-app">
-            <div className="search">
-                <div className="search-top">
-                    <i className="fa-solid fa-location"></i>
-                    <div className="location">London</div>
-                </div>
-                <div className="search-bar">
-                    <input type="text" name="" id="" placeholder="Enter Location" />
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                </div>
-            </div>
+    <div className="container" style={{ backgroundImage }}>
+      <div
+        className="weather-app"
+        style={{
+          backgroundImage:
+            backgroundImage && backgroundImage.replace
+              ? backgroundImage.replace("to right", "to top")
+              : "null",
+        }}
+      >
+        <div className="search">
+          <div className="search-top">
+            <i className="fa-solid fa-location"></i>
+            <div className="location">{data.name}</div>
+          </div>
+          <div className="search-bar">
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter Location"
+              value={location}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
+          </div>
+        </div>
+        {loading ? (<img className="loader" src={loadingGif} alt="loading" />) : data.notFound ? (
+          <div className="not-found">Not Found 😒</div>
+        ) : (
+          <>
             <div className="weather">
-                  <img src={sunny} alt='sunny' />
-                  <div className="weather-type">
-                    clear
-                  </div>
-                  <div className="temp">28°</div>
+              <img src={weatherImage} alt="sunny" />
+              <div className="weather-type">
+                {data.weather ? data.weather[0].main : null}
+              </div>
+              <div className="temp">
+                {data.main ? `${Math.floor(data.main.temp)}°` : null}
+              </div>
             </div>
             <div className="weather-date">
-                <p>Fri, 3 May</p>
+              <p>{currentDate}</p>
             </div>
             <div className="weather-data">
-                <div className="humidity">
-                    <div className="data-name">Humidity</div>
-                     <i className="fa-solid fa-droplet"></i>
-                     <div className="data">35%</div>
+              <div className="humidity">
+                <div className="data-name">Humidity</div>
+                <i className="fa-solid fa-droplet"></i>
+                <div className="data">
+                  {data.main ? data.main.humidity : null}%
                 </div>
-                  <div className="wind">
-                    <div className="data-name">Wind</div>
-                     <i className="fa-solid fa-wind"></i>
-                     <div className="data">3 km/h</div>
+              </div>
+              <div className="wind">
+                <div className="data-name">Wind</div>
+                <i className="fa-solid fa-wind"></i>
+                <div className="data">
+                  {data.wind ? data.wind.speed : null} km/h
                 </div>
+              </div>
             </div>
-        </div>
+          </>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherApp
+export default WeatherApp;
